01cd03e71e7c83e0eb4db6c8c9da856c24ba0342
grodriguez.xsi <grodriguez.xsi@gmail.com>
2026-03-17 15:01:46 +0000

    Revert "Soluciona errores de conexión y compilación en Android"
    This reverts commit dc655c2e6d04766d1d6a7b4229f2080a24ca1a33.

--- lib/constantes.dart
+++ lib/constantes.dart
@@ -66,7 +66,7 @@ class ListasAutocompletado {
     "KWID", "MASTER", "CLIO",
     // CHEVROLET
     "ONIX", "PRISMA", "TRACKER", "CRUZE", "S10", "EQUINOX", "JOY", "SPIN",
-    "MONTANA", "CORSA", "CELTA",
+    "MONTANA", "CORSA",
     // PEUGEOT
     "208", "2008", "3008", "308", "408", "PARTNER", "EXPERT", "BOXER",
     // CITROEN
